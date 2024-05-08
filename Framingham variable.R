8b99df0bad0caf1cae14fe060cf915f1a30fcfb6
rachmorse <86972143+rachmorse@users.noreply.github.com>
2024-05-08 16:26:55 +0200

    Cleaned code

--- Framingham variable.R
+++ Framingham variable.R
@@ -1,173 +1,190 @@
 
-#### CREATING ADJUSTED FRS ####
-if (!require("tidyverse")) {install.packages("tidyverse"); require("tidyverse")}
+# Load packages
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+  require("tidyverse")
+}
+
+if (!require("psych")) {
+  install.packages("psych")
+  require("psych")
+}
 
 library(readxl)
-getwd()  
+
+# Read in the data 
+getwd()
 setwd("/Users/rachelmorse/Documents/2020:2021/Dissertation/Data")
-fr1 <- read_excel("Framingham Data.xlsx", 
-                  sheet = "Data fr Adjusted", col_types = c("text", 
-                                                            "numeric", "numeric", "numeric", 
-                                                            "numeric", "numeric", "numeric", "numeric", 
-                                                            "numeric"))
-attach(fr1)
-fr1 <- fr1 %>% 
-  #age points
-  filter(!is.na(Age)) %>% 
-  mutate(agefr = case_when((Age <= 64.99999 & Sex == 0) ~ 10,
-                           (Age <= 64.99999 & Sex == 1) ~ 10,
-                           (Age > 64.99999 & Age <= 69.99999 & Sex == 0) ~ 11,
-                           (Age > 64.99999 & Age <= 69.99999 & Sex == 1) ~ 12,
-                           (Age > 69.99999 & Age <= 74.99999 & Sex == 0) ~ 12,
-                           (Age > 69.99999 & Age <= 74.99999 & Sex == 1) ~ 14,
-                           (Age > 74.99999 & Sex == 0) ~ 13,
-                            TRUE ~ 16)) %>% 
+fr1 <- read_excel("Framingham Data.xlsx",
+  sheet = "Data fr Adjusted", col_types = c(
+    "text",
+    rep("numeric", 8)
+  )
+)
 
-  #SBP points  
-  filter(!is.na(SBP)) %>% 
-  mutate(sbpfr = case_when(SBP <= 120 ~ 0,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 0 & Sex == 0) ~ 0,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 0 & Sex == 1) ~ 1,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 1 & Sex == 0) ~ 1,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 1 & Sex == 1) ~ 3,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 0 & Sex == 0) ~ 1,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 0 & Sex == 1) ~ 2,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 1 & Sex == 0) ~ 2,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 1 & Sex == 1) ~ 4,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 0 & Sex == 0) ~ 1,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 0 & Sex == 1) ~ 3,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 1 & Sex == 0) ~ 2,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 1 & Sex == 1) ~ 5,
-                           (SBP >= 160 & BP.Medication == 0 & Sex == 0) ~ 2,
-                           (SBP >= 160 & BP.Medication == 0 & Sex == 1) ~ 4,
-                           (SBP >= 160 & BP.Medication == 1 & Sex == 0) ~ 3,
-                           TRUE ~ 6)) %>% 
-  
-  #cholesterol points
-  filter(!is.na(Cholesterol)) %>% 
-  mutate(chfr = case_when((Cholesterol == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
-                          (Cholesterol == 1 & Age <= 69.99999 & Sex == 1) ~ 1,
-                          (Cholesterol == 1 & Age > 69.99999 & Sex == 0) ~ 0,
-                          (Cholesterol == 1 & Age > 69.99999 & Sex == 1) ~ 0, 
-                            TRUE ~ 0)) %>%
-  #smoker points
-  filter(!is.na(Smoker)) %>% 
-  mutate(smokefr = case_when((Smoker == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
-                             (Smoker == 1 & Age <= 69.99999 & Sex == 1) ~ 2,
-                             (Smoker == 1 & Age > 69.99999 & Sex == 0) ~ 1,
-                             (Smoker == 1 & Age > 69.99999 & Sex == 1) ~ 1,
-                               TRUE ~ 0)) %>%
-  
+# This code is used to create the Framingham Risk Score Adjusted called "frs1" in the df "fr1"
+fr1 <- fr1 %>%
+  # age points
+  filter(!is.na(Age)) %>%
+  mutate(agefr = case_when(
+    (Age <= 64.99999 & Sex == 0) ~ 10,
+    (Age <= 64.99999 & Sex == 1) ~ 10,
+    (Age > 64.99999 & Age <= 69.99999 & Sex == 0) ~ 11,
+    (Age > 64.99999 & Age <= 69.99999 & Sex == 1) ~ 12,
+    (Age > 69.99999 & Age <= 74.99999 & Sex == 0) ~ 12,
+    (Age > 69.99999 & Age <= 74.99999 & Sex == 1) ~ 14,
+    (Age > 74.99999 & Sex == 0) ~ 13,
+    TRUE ~ 16
+  )) %>%
+  # SBP points
+  filter(!is.na(SBP)) %>%
+  mutate(sbpfr = case_when(
+    SBP <= 120 ~ 0,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 0 & Sex == 0) ~ 0,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 0 & Sex == 1) ~ 1,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 1 & Sex == 0) ~ 1,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 1 & Sex == 1) ~ 3,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 0 & Sex == 0) ~ 1,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 0 & Sex == 1) ~ 2,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 1 & Sex == 0) ~ 2,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 1 & Sex == 1) ~ 4,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 0 & Sex == 0) ~ 1,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 0 & Sex == 1) ~ 3,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 1 & Sex == 0) ~ 2,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 1 & Sex == 1) ~ 5,
+    (SBP >= 160 & "BP Medication" == 0 & Sex == 0) ~ 2,
+    (SBP >= 160 & "BP Medication" == 0 & Sex == 1) ~ 4,
+    (SBP >= 160 & "BP Medication" == 1 & Sex == 0) ~ 3,
+    TRUE ~ 6
+  )) %>%
+  # cholesterol points
+  filter(!is.na(Cholesterol)) %>%
+  mutate(chfr = case_when(
+    (Cholesterol == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
+    (Cholesterol == 1 & Age <= 69.99999 & Sex == 1) ~ 1,
+    (Cholesterol == 1 & Age > 69.99999 & Sex == 0) ~ 0,
+    (Cholesterol == 1 & Age > 69.99999 & Sex == 1) ~ 0,
+    TRUE ~ 0
+  )) %>%
+  # smoker points
+  filter(!is.na(Smoker)) %>%
+  mutate(smokefr = case_when(
+    (Smoker == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
+    (Smoker == 1 & Age <= 69.99999 & Sex == 1) ~ 2,
+    (Smoker == 1 & Age > 69.99999 & Sex == 0) ~ 1,
+    (Smoker == 1 & Age > 69.99999 & Sex == 1) ~ 1,
+    TRUE ~ 0
+  )) %>%
+  mutate(frs1 = agefr + chfr + smokefr + sbpfr)
 
-  mutate(frs1 = agefr + chfr + smokefr + sbpfr) 
-
-#### CREATING UNADJUSTED FRS ####
-
-fr2 <- read_excel("Framingham Data.xlsx", 
-                 sheet = "Data for Complete", col_types = c("text", "numeric", "numeric",
-                                                           "numeric", "numeric", "numeric", 
-                                                           "numeric", "numeric", "numeric", "numeric"))
-attach(fr2)
-
-fr2 <- data.frame(fr2)
+# This code is used to create the Framingham Risk Score Unadjusted called "frs2" in the df "fr2"
+fr2 <- read_excel("Framingham Data.xlsx",
+  sheet = "Data for Complete", col_types = c(
+    "text", 
+    rep("numeric", 9)
+  )
+)
 
 fr2 <- fr2 %>% 
-  #age points
-  mutate(agefr2 = case_when((Age <= 64.99999 & Sex == 0) ~ 10,
-                           (Age <= 64.99999 & Sex == 1) ~ 10,
-                           (Age > 64.99999 & Age <= 69.99999 & Sex == 0) ~ 11,
-                           (Age > 64.99999 & Age <= 69.99999 & Sex == 1) ~ 12,
-                           (Age >  69.99999 & Age <= 74.99999 & Sex == 0) ~ 12,
-                           (Age > 69.99999 & Age <= 74.99999 & Sex == 1) ~ 14,
-                           (Age > 74.99999 & Sex == 0) ~ 13,
-                           TRUE ~ 16)) %>% 
-  #SBP points
-  filter(!is.na(SBP)) %>% 
-  mutate(sbpfr2 = case_when(SBP <= 120 ~ 0,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 0 & Sex == 0) ~ 0,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 0 & Sex == 1) ~ 1,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 1 & Sex == 0) ~ 1,
-                           (SBP > 120 & SBP <= 129 & BP.Medication == 1 & Sex == 1) ~ 3,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 0 & Sex == 0) ~ 1,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 0 & Sex == 1) ~ 2,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 1 & Sex == 0) ~ 2,
-                           (SBP > 129 & SBP <= 139 & BP.Medication == 1 & Sex == 1) ~ 4,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 0 & Sex == 0) ~ 1,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 0 & Sex == 1) ~ 3,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 1 & Sex == 0) ~ 2,
-                           (SBP > 139 & SBP <= 159 & BP.Medication == 1 & Sex == 1) ~ 5,
-                           (SBP >= 160 & BP.Medication == 0 & Sex == 0) ~ 2,
-                           (SBP >= 160 & BP.Medication == 0 & Sex == 1) ~ 4,
-                           (SBP >= 160 & BP.Medication == 1 & Sex == 0) ~ 3,
-                           TRUE ~ 6)) %>% 
-  
-  #total cholesterol points
-  mutate(chmgdl = (Total.Cholesterol * 38.67)) %>% #convert from mmol/L to mg/dL
-  
-  mutate(chfr2 = case_when(chmgdl <= 160 ~ 0,
-            (chmgdl > 160 & chmgdl <= 199 & Age <= 69.99999 & Sex == 0) ~ 1,
-            (chmgdl > 160 & chmgdl <= 199 & Age <= 69.99999 & Sex == 1) ~ 1,
-            (chmgdl > 160 & chmgdl <= 199 & Age > 69.99999 & Sex == 0) ~ 0,
-            (chmgdl > 160 & chmgdl <= 199 & Age > 69.99999 & Sex == 1) ~ 1,
-            (chmgdl > 199 & chmgdl <= 239 & Age <= 69.99999 & Sex == 0) ~ 1,
-            (chmgdl > 199 & chmgdl <= 239 & Age <= 69.99999 & Sex == 1) ~ 2,
-            (chmgdl > 199 & chmgdl <= 239 & Age > 69.99999 & Sex == 0) ~ 0,
-            (chmgdl > 199 & chmgdl <= 239 & Age > 69.99999 & Sex == 1) ~ 1,
-            (chmgdl > 239 & chmgdl <= 279 & Age <= 69.99999 & Sex == 0) ~ 2,
-            (chmgdl > 239 & chmgdl <= 279 & Age <= 69.99999 & Sex == 1) ~ 3,
-            (chmgdl > 239 & chmgdl <= 279 & Age > 69.99999 & Sex == 0) ~ 1,
-            (chmgdl > 239 & chmgdl <= 279 & Age > 69.99999 & Sex == 1) ~ 2,
-            (chmgdl > 279 & Age <= 69.99999 & Sex == 0) ~ 3,
-            (chmgdl > 279 & Age <= 69.99999 & Sex == 1) ~ 4,
-            (chmgdl > 279 & Age > 69.99999 & Sex == 0) ~ 1,
-            TRUE ~ 2))%>% 
- 
-  #hdl cholesterol points
-  mutate(hdlmgdl = (HDL.Cholesterol * 38.67)) %>% 
-  
-  filter(!is.na(hdlmgdl)) %>% 
-  mutate(hdlfr2 = case_when(hdlmgdl >= 60 ~ -1,
-                            (hdlmgdl < 60 & hdlmgdl >= 50) ~ 0,
-                            (hdlmgdl < 50 & hdlmgdl >= 40) ~ 1,
-                            TRUE ~ 2)) %>% 
-  #smoker points
-  filter(!is.na(Smoker)) %>% 
-  mutate(smokefr2 = case_when((Smoker == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
-                             (Smoker == 1 & Age <= 69.99999 & Sex == 1) ~ 2,
-                             (Smoker == 1 & Age > 69.99999 & Sex == 0) ~ 1,
-                             (Smoker == 1 & Age > 69.99999 & Sex == 1) ~ 1,
-                             TRUE ~ 0)) %>%
-  
-  mutate(frs2 = agefr2 + chfr2 + hdlfr2 + smokefr2 + sbpfr2) 
+  rename(
+    Age = "age",
+    Sex = "sex",
+    Smoker = "smoker",
+    "BP Medication" = "bp treatment",
+    SBP = "sbp",
+    Total.Cholesterol = "total cholesterol",
+    HDL.Cholesterol = "hdl cholesterol"
+    )
 
+fr2 <- fr2 %>%
+  # age points
+  mutate(agefr2 = case_when(
+    (Age <= 64.99999 & Sex == 0) ~ 10,
+    (Age <= 64.99999 & Sex == 1) ~ 10,
+    (Age > 64.99999 & Age <= 69.99999 & Sex == 0) ~ 11,
+    (Age > 64.99999 & Age <= 69.99999 & Sex == 1) ~ 12,
+    (Age > 69.99999 & Age <= 74.99999 & Sex == 0) ~ 12,
+    (Age > 69.99999 & Age <= 74.99999 & Sex == 1) ~ 14,
+    (Age > 74.99999 & Sex == 0) ~ 13,
+    TRUE ~ 16
+  )) %>%
+  # SBP points
+  filter(!is.na(SBP)) %>%
+  mutate(sbpfr2 = case_when(
+    SBP <= 120 ~ 0,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 0 & Sex == 0) ~ 0,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 0 & Sex == 1) ~ 1,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 1 & Sex == 0) ~ 1,
+    (SBP > 120 & SBP <= 129 & "BP Medication" == 1 & Sex == 1) ~ 3,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 0 & Sex == 0) ~ 1,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 0 & Sex == 1) ~ 2,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 1 & Sex == 0) ~ 2,
+    (SBP > 129 & SBP <= 139 & "BP Medication" == 1 & Sex == 1) ~ 4,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 0 & Sex == 0) ~ 1,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 0 & Sex == 1) ~ 3,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 1 & Sex == 0) ~ 2,
+    (SBP > 139 & SBP <= 159 & "BP Medication" == 1 & Sex == 1) ~ 5,
+    (SBP >= 160 & "BP Medication" == 0 & Sex == 0) ~ 2,
+    (SBP >= 160 & "BP Medication" == 0 & Sex == 1) ~ 4,
+    (SBP >= 160 & "BP Medication" == 1 & Sex == 0) ~ 3,
+    TRUE ~ 6
+  )) %>%
+  # total cholesterol points
+  mutate(chmgdl = (Total.Cholesterol * 38.67)) %>% # convert from mmol/L to mg/dL
 
-if (!require("psych")) {install.packages("psych"); require("psych")}
+  mutate(chfr2 = case_when(
+    chmgdl <= 160 ~ 0,
+    (chmgdl > 160 & chmgdl <= 199 & Age <= 69.99999 & Sex == 0) ~ 1,
+    (chmgdl > 160 & chmgdl <= 199 & Age <= 69.99999 & Sex == 1) ~ 1,
+    (chmgdl > 160 & chmgdl <= 199 & Age > 69.99999 & Sex == 0) ~ 0,
+    (chmgdl > 160 & chmgdl <= 199 & Age > 69.99999 & Sex == 1) ~ 1,
+    (chmgdl > 199 & chmgdl <= 239 & Age <= 69.99999 & Sex == 0) ~ 1,
+    (chmgdl > 199 & chmgdl <= 239 & Age <= 69.99999 & Sex == 1) ~ 2,
+    (chmgdl > 199 & chmgdl <= 239 & Age > 69.99999 & Sex == 0) ~ 0,
+    (chmgdl > 199 & chmgdl <= 239 & Age > 69.99999 & Sex == 1) ~ 1,
+    (chmgdl > 239 & chmgdl <= 279 & Age <= 69.99999 & Sex == 0) ~ 2,
+    (chmgdl > 239 & chmgdl <= 279 & Age <= 69.99999 & Sex == 1) ~ 3,
+    (chmgdl > 239 & chmgdl <= 279 & Age > 69.99999 & Sex == 0) ~ 1,
+    (chmgdl > 239 & chmgdl <= 279 & Age > 69.99999 & Sex == 1) ~ 2,
+    (chmgdl > 279 & Age <= 69.99999 & Sex == 0) ~ 3,
+    (chmgdl > 279 & Age <= 69.99999 & Sex == 1) ~ 4,
+    (chmgdl > 279 & Age > 69.99999 & Sex == 0) ~ 1,
+    TRUE ~ 2
+  )) %>%
+  # hdl cholesterol points
+  mutate(hdlmgdl = (HDL.Cholesterol * 38.67)) %>%
+  filter(!is.na(hdlmgdl)) %>%
+  mutate(hdlfr2 = case_when(
+    hdlmgdl >= 60 ~ -1,
+    (hdlmgdl < 60 & hdlmgdl >= 50) ~ 0,
+    (hdlmgdl < 50 & hdlmgdl >= 40) ~ 1,
+    TRUE ~ 2
+  )) %>%
+  # smoker points
+  filter(!is.na(Smoker)) %>%
+  mutate(smokefr2 = case_when(
+    (Smoker == 1 & Age <= 69.99999 & Sex == 0) ~ 1,
+    (Smoker == 1 & Age <= 69.99999 & Sex == 1) ~ 2,
+    (Smoker == 1 & Age > 69.99999 & Sex == 0) ~ 1,
+    (Smoker == 1 & Age > 69.99999 & Sex == 1) ~ 1,
+    TRUE ~ 0
+  )) %>%
+  mutate(frs2 = agefr2 + chfr2 + hdlfr2 + smokefr2 + sbpfr2)
 
-## frs adjusted ##
-frs1 <- (fr1$frs1)
-describe(frs1)
+## Descriptive stats for frs1 (the adjusted score) ##
+describe(fr1$frs1)
 
-agewellfrs <- slice(Data_Summary1, 1:113)
+## Descriptive stats for frs2 (the unadjusted score) ##
+describe(fr2$frs2)
 
-scdwellfrs <- slice(Data_Summary1, 114:260)
+# Subset the dfs to only ID and FRS to merge
+fr1 <- select(fr1, ID, frs1)
+fr2 <- select(fr2, ID, frs2)
 
-## frs unajdusted ##
-frs2 <- (fr2$frs2)
-describe(frs2)
+# Merge the dfs for a correlation analysis
+framingham <- merge(fr1, fr2, by = "ID")
 
-framingham <- merge(fr2,fr1,by="ID")
-attach(framingham)
-
-
-### CORRELATION FRS ADJ & UNADJ 
-if(!require(devtools)) install.packages("devtools")
-
-corr_fr <- cor.test(framingham$fr2,framingham$fr1, method=("pearson"))
-conf_int_fr <- corr_fr$conf.int
-p_fr <- corr_fr$p.value
-
-cor.test(framingham$fr2,framingham$fr1, method=("pearson"))
-print(paste(conf_int_fr[1], "-", conf_int_fr[2]))
-print(paste(p_fr))
-
-
+# Calculate the Pearson correlation
+corr_frs <- cor.test(framingham$frs1, framingham$frs2, method = "pearson")
+print(paste(corr_frs$conf.int[1], "-", corr_frs$conf.int[2]))
+print(paste(corr_frs$p.value))
--- PACC5 variable.R
+++ PACC5 variable.R
@@ -1,8 +1,8 @@
 ###############################################
 ### Creating PACC-5 for Age-Well & SCD-Well ###
 ###############################################
-install.packages("styler")
 
+# Load packages
 if (!require("tidyverse")) {
   install.packages("tidyverse")
   require("tidyverse")
@@ -13,127 +13,121 @@ if (!require("psych")) {
 }
 
 library(readxl)
+
+# Read in data 
 data <- read_excel("~/Documents/2021:2022/Marchant Lab/Data/MS Data.xlsx",
   col_types = c(
-    "text", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "text", "numeric",
-    "text", "text", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric", "numeric",
-    "numeric", "numeric"
+    "text", 
+    rep("numeric", 19),
+    "text", "numeric",
+    rep("text", 2),
+    rep("numeric", 10)
   )
 )
-attach(data)
 
+# Divide the data by cohort 
 ageonlypacc <- slice(data, 148:284)
-
 scdonlypacc <- slice(data, 1:147)
 
+###################################################################
 ## Creating adjusted PACC scores for combined cohort analysis ##
-attach(ageonlypacc)
-ageonlypacc$cvlt <- (`ravlt OR cvlt (in Age-Well) this column can only be use for separate cohort analyses`)
+###################################################################
 
+# Rename CVLT in Age-Well 
+ageonlypacc <- ageonlypacc %>% 
+  rename(cvlt = `ravlt OR cvlt (in Age-Well) this column can only be use for separate cohort analyses`)
+
+# Transform CVLT in Age-Well to be comparable with RAVLT in SCD-Well and name it cvlt_ravlt
 ageonlypacc <- ageonlypacc %>%
   filter(!is.na(cvlt)) %>%
-  mutate(cvlt_rvlt = (cvlt / 16 * 15))
+  mutate(cvlt_ravlt = (cvlt / 16 * 15))
 
-attach(scdonlypacc)
-scdonlypacc$ravlt <- (`ravlt OR cvlt (in Age-Well) this column can only be use for separate cohort analyses`)
-scdonlypacc$cvlt_rvlt <- scdonlypacc$ravlt
+# Rename RAVLT in SCD-Well 
+scdonlypacc <- scdonlypacc %>% 
+  rename(ravlt = `ravlt OR cvlt (in Age-Well) this column can only be use for separate cohort analyses`)
 
-merged_df <- full_join(scdonlypacc, ageonlypacc, by = c("ID", "drs", "cvlt_rvlt", "coding", "fluency"))
+# Then create a new variable cvlt-ravlt to be able to compare cohorts
+scdonlypacc$cvlt_ravlt <- scdonlypacc$ravlt
 
-merged_df <- merged_df[complete.cases(
-  merged_df$drs, merged_df$cvlt_rvlt, merged_df$fluency,
-  merged_df$coding
-), ]
+# Subset the df to needed columns and merge the cohorts 
+columns_for_analysis <- c("ID", "drs", "cvlt_ravlt", "coding", "fluency") # Set list of columns
 
-## Standardise components (create z-scores) ##
-merged_df$zdrs <- scale(merged_df$drs)
-merged_df$zcvlt_rvlt <- scale(merged_df$cvlt_rvlt)
-merged_df$zfluency <- scale(merged_df$fluency)
-merged_df$zcoding <- scale(merged_df$coding)
+merged_df <- full_join(scdonlypacc, ageonlypacc, by = columns_for_analysis) # Merge
 
-## Create a matrix of the Z scores ##
-zScores <- cbind(merged_df$zdrs, merged_df$zcvlt_rvlt, merged_df$zfluency, merged_df$zcoding)
+merged_df <- merged_df %>% 
+  select(columns_for_analysis) # Subset df to list of columns
 
-## Average standardised components to create the PACC-5 ##
-merged_df$pacc5 <- rowMeans(zScores[, 1:4], na.rm = TRUE)
-rm(zScores)
-
-merged_df$zpacc5 <- scale(merged_df$pacc5)
-
-## CREATING SCD ADJUSTED PACC ##
+merged_df <- merged_df[complete.cases(merged_df[columns_for_analysis]), ] # Remove NAs
 
 ## Standardise components (create z-scores) ##
-scdonlypacc$zdrs <- scale(scdonlypacc$drs)
-scdonlypacc$zravlt <- scale(scdonlypacc$ravlt)
-scdonlypacc$zfluency <- scale(scdonlypacc$fluency)
-scdonlypacc$zcoding <- scale(scdonlypacc$coding)
+cols_to_scale <- c("zdrs" = "drs", "zcvlt_ravlt" = "cvlt_ravlt", "zfluency" = "fluency", "zcoding" = "coding") # New names are labeled with z
 
-## Create a matrix of the Z scores ##
-zScores_scdonlypacc <- cbind(scdonlypacc$zdrs, scdonlypacc$zravlt, scdonlypacc$zfluency, scdonlypacc$zcoding)
+merged_df <- merged_df %>%
+  mutate(across(cols_to_scale, scale, .names = "{.col}"))
 
-## Average standardised components to create the PACC-5 ##
-scdonlypacc$pacc5 <- rowMeans(zScores_scdonlypacc[, 1:4], na.rm = TRUE)
-rm(zScores_scdonlypacc)
+## Create the PACC scores ##
+merged_df <- merged_df %>%
+  mutate(
+    pacc5 = rowMeans(select(., zdrs, zcvlt_ravlt, zfluency, zcoding)), # Calculate the mean for each row
+    zpacc5 = scale(pacc5) # Scale the PACC variable
+  )  
 
-scdonlypacc$zpacc5 <- scale(scdonlypacc$pacc5)
+###########################################
+## Creating adjusted PACC scores for SCD ##
+###########################################
 
-## CREATING UNADJUSTED AGE PACC ## ----
-attach(ageonlypacc)
+## Standardise components (create z-scores) ##
+cols_to_scale_scd <- c("zdrs" = "drs", "zravlt" = "ravlt", "zfluency" = "fluency", "zcoding" = "coding") # New names are labeled with z
+
+scdonlypacc <- scdonlypacc %>%
+  mutate(across(cols_to_scale_scd, scale, .names = "{.col}"))
+
+## Create the PACC scores ##
+scdonlypacc <- scdonlypacc %>%
+  mutate(
+    pacc5 = rowMeans(select(., zdrs, zravlt, zfluency, zcoding)), # Calculate the mean for each row
+    zpacc5 = scale(pacc5) # Scale the PACC variable
+  )  
+
+##################################################
+## Creating UNadjusted PACC scores for Age-Well ##
+##################################################
+# Note that this is a 5 component (with logical mem) PACC whereas the others are adjusted as they only include 4 components
+
+# Rename variables
 ageonlypacc$log <- (`logical mem`)
 
 ## Standardise components (create z-scores) ##
-ageonlypacc$zdrs <- scale(ageonlypacc$drs)
-ageonlypacc$zcvlt <- scale(ageonlypacc$cvlt)
-ageonlypacc$zfluency <- scale(ageonlypacc$fluency)
-ageonlypacc$zcoding <- scale(ageonlypacc$coding)
-ageonlypacc$zlog <- scale(ageonlypacc$log)
+cols_to_scale_age <- c("zdrs" = "drs", "zcvlt" = "cvlt", "zfluency" = "fluency", "zcoding" = "coding", "zlog" = "log") # New names are labeled with z
 
+ageonlypacc <- ageonlypacc %>%
+  mutate(across(cols_to_scale_age, scale, .names = "{.col}"))
 
-## Create a matrix of the Z scores ##
-zScores_ageonlypacc <- cbind(ageonlypacc$zdrs, ageonlypacc$zcvlt, ageonlypacc$zfluency, ageonlypacc$zcoding, ageonlypacc$zlog)
+## Create the PACC scores ##
+ageonlypacc <- ageonlypacc %>%
+  mutate(
+    unadj_pacc5 = rowMeans(select(., zdrs, cvlt, zfluency, zcoding, zlog)), # Calculate the mean for each row
+    unadj_zpacc5 = scale(unadj_pacc5) # Scale the PACC variable
+  )  
 
-## Average standardised components to create the PACC-5 ##
-ageonlypacc$pacc5 <- rowMeans(zScores_ageonlypacc[, 1:5], na.rm = TRUE)
-rm(zScores_ageonlypacc)
-
-ageonlypacc$zpacc5 <- scale(ageonlypacc$pacc5)
-
-as.numeric(ageonlypacc$zpacc5)
-describe(ageonlypacc$zpacc5)
-
-## CREATING ADJUTSED AGE PACC ## ----
+################################################
+## Creating adjusted PACC scores for Age-Well ##
+################################################
 
 ## Standardise components (create z-scores) ##
-ageonlypacc$zdrs <- scale(ageonlypacc$drs)
-ageonlypacc$zcvlt <- scale(ageonlypacc$cvlt)
-ageonlypacc$zfluency <- scale(ageonlypacc$fluency)
-ageonlypacc$zcoding <- scale(ageonlypacc$coding)
+cols_to_scale_age_adj <- c("zdrs" = "drs", "zcvlt" = "cvlt", "zfluency" = "fluency", "zcoding" = "coding") # New names are labeled with z
 
-## Create a matrix of the Z scores ##
-zScores_ageonlyadjpacc <- cbind(ageonlypacc$zdrs, ageonlypacc$zcvlt, ageonlypacc$zfluency, ageonlypacc$zcoding)
+ageonlypacc <- ageonlypacc %>%
+  mutate(across(cols_to_scale_age_adj, scale, .names = "{.col}"))
 
-## Average standardised components to create the PACC-5 ##
-ageonlypacc$adjpacc5 <- rowMeans(zScores_ageonlyadjpacc[, 1:4], na.rm = TRUE)
-rm(zScores_ageonlyadjpacc)
+## Create the PACC scores ##
+ageonlypacc <- ageonlypacc %>%
+  mutate(
+    pacc5 = rowMeans(select(., zdrs, cvlt, zfluency, zcoding)), # Calculate the mean for each row
+    zpacc5 = scale(pacc5) # Scale the PACC variable
+  ) 
 
-ageonlypacc$adjzpacc5 <- scale(ageonlypacc$adjpacc5)
-
-### CORRELATION ADJ UNADJ PACC
-if (!require(devtools)) install.packages("devtools")
-cor.test(ageonlypacc$adjzpacc5, ageonlypacc$zpacc5, method = ("pearson"))
-
-corr_pacc <- cor.test(ageonlypacc$adjzpacc5, ageonlypacc$zpacc5, method = ("pearson"))
-conf_int_pacc <- corr_pacc$conf.int
-p_cci <- corr_pacc$p.value
-
-cor.test(ageonlypacc$adjzpacc5, ageonlypacc$zpacc5, method = ("pearson"))
-print(paste(conf_int_pacc[1], "-", conf_int_pacc[2]))
-print(paste(p_cci))
+# Calculate the Pearson correlation for adjusted and unadjusted PACC
+corr_pacc <- cor.test(ageonlypacc$unadj_zpacc5, ageonlypacc$zpacc5, method = "pearson")
+print(paste(corr_pacc$conf.int[1], "-", corr_pacc$conf.int[2]))
+print(paste(corr_pacc$p.value))
